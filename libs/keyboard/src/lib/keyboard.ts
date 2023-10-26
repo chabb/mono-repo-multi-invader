dcdf80e886715b35bba8ceaa469f6c4ff736f557
U120655 <U120655@mobi.ch>
2023-10-26 21:16:22 +0200

    keyboard lib is done

--- libs/keyboard/src/lib/keyboard.ts
+++ libs/keyboard/src/lib/keyboard.ts
@@ -1,3 +1,41 @@
-export function keyboard(): string {
-  return 'keyboard';
+export function keyboard(keyCode: number) {
+  const key: Key = {
+    keyCode: keyCode,
+    isDown: false,
+    isUp: true,
+    press: undefined,
+    release: undefined,
+    downHandler: (event) => {
+      if (event.keyCode === key.keyCode) {
+        if (key.isUp && key.press) {
+          key.press();
+        }
+        key.isDown = true;
+        key.isUp = false;
+      }
+      event.preventDefault()
+    }, upHandler: (event) => {
+      if (event.keyCode === key.keyCode) {
+        if (key.isDown && key.release) {
+          key.release();
+        }
+        key.isDown = false;
+        key.isUp = true;
+
+      }
+    }
+  };
+  window.addEventListener('keydown', key.downHandler.bind(key), false);
+  window.addEventListener('keyup', key.upHandler.bind(key), false);
+  return key;
+}
+
+export interface Key {
+  isDown: boolean;
+  isUp: boolean;
+  keyCode: number;
+  downHandler: (event: KeyboardEvent) => void;
+  upHandler: (event: KeyboardEvent) => void;
+  press?: () => void;
+  release?: () => void;
 }
