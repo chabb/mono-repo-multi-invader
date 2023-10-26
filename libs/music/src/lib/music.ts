b3fe1ee315c2329c1dc70e926644b4559d8eb738
U120655 <U120655@mobi.ch>
2023-10-26 21:09:14 +0200

    music lib is done

--- libs/music/src/lib/music.ts
+++ libs/music/src/lib/music.ts
@@ -1,3 +1,24 @@
-export function music(): string {
-  return 'music';
+
+// ugly code to play music
+let audioBuffer: AudioBuffer
+const context: AudioContext = new AudioContext();
+const getSound: XMLHttpRequest = new XMLHttpRequest();
+getSound.responseType = 'arraybuffer';
+getSound.onload = function() {
+  context.decodeAudioData(getSound.response).then(buffer => {
+    audioBuffer = buffer; // assign the buffer to a variable that can then be 'played'
+    playSound();
+  })
+};
+getSound.send();
+
+function playSound() {
+  const playSound = context.createBufferSource();
+  playSound.buffer = audioBuffer;
+  playSound.connect(context.destination);
+  playSound.start(0)
+}
+
+export function loadMusic(file: string | URL) {
+  getSound.open("GET", file, true);
 }
