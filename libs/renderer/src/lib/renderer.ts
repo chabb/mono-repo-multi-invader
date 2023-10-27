c0f6d042e23f41dc41e95d1c8dfdfdff40316b9b
U120655 <U120655@mobi.ch>
2023-10-27 09:58:24 +0200

    fix(renderer): fix renderer types

--- libs/renderer/src/lib/renderer.ts
+++ libs/renderer/src/lib/renderer.ts
@@ -1,3 +1,20 @@
-export function renderer(): string {
-  return 'renderer';
+interface CanvasWithContext extends HTMLCanvasElement {
+  ctx: CanvasRenderingContext2D
+}
+
+export function makeCanvas(width = 256, heigth = 256, border = '1px dashed black', backgroundColor = 'white'): CanvasWithContext {
+  const canvas: HTMLCanvasElement = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = heigth;
+  canvas.style.border = border;
+  canvas.style.backgroundColor = backgroundColor;
+  document.body.appendChild(canvas);
+  // not a good practice;
+  const ctx = canvas.getContext('2d');
+  if (ctx) {
+    const canvasWithContext: CanvasWithContext = Object.assign(canvas, { ctx });
+    return canvasWithContext;
+  } else {
+    throw new Error('unable to get canvas context');
+  }
 }
