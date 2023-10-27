dfb36127a293ad77c4e5afd69f2c8f50d1630437
U120655 <U120655@mobi.ch>
2023-10-27 09:57:46 +0200

    feat(sprite): add group

--- libs/sprite/src/lib/sprite.ts
+++ libs/sprite/src/lib/sprite.ts
@@ -263,5 +263,52 @@ export class Rectangle extends DisplayObject {
         if (this.fillStyle !== 'none') ctx.fill();
         if (this.mask && this.mask === true) ctx.clip();
     }
+}
 
+// a group must be able to compute its height/width on the fly
+export class Group extends DisplayObject {
+
+  private _newWidth!: number;
+  private _newHeight!: number;
+
+  constructor(...spritesToGroup: DisplayObject[]) {
+    super();
+    spritesToGroup.forEach(sprite => this.addChild(sprite));
+  }
+
+  override addChild(sprite: DisplayObject): void {
+    if (sprite.parent) {
+      sprite.parent.removeChild(sprite);
+    }
+    sprite.parent = this;
+    this.children.push(sprite);
+    this.calculateSize();
+  }
+  override removeChild(sprite: DisplayObject): void {
+
+    if(sprite.parent === this) {
+      this.children.splice(this.children.indexOf(sprite), 1);
+      this.calculateSize();
+    } else {
+      throw new Error(`${sprite} not a child of ${this}`);
+    }
+  }
+
+  calculateSize() {
+    if (this.children.length > 0) {
+      this._newHeight = 0;
+      this._newWidth = 0;
+      this.children.forEach(child => {
+        if (child.x + child.width > this._newWidth) {
+          this._newWidth = child.x + child.width;
+        }
+        if (child.y + child.height > this._newHeight) {
+          this._newHeight = child.y + child.height;
+        }
+      });
+    }
+
+    this.width = this._newWidth;
+    this.height = this._newHeight;
+  }
 }
