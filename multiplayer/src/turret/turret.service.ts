77052e4348659017ed1c4dbc01efffa96e7a4112
U120655 <U120655@mobi.ch>
2023-11-05 13:30:58 +0100

    fix: use correct service

--- multiplayer/src/turret/turret.service.ts
+++ multiplayer/src/turret/turret.service.ts
@@ -1,20 +1,20 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { Turret } from '@chabb/shared';
+import { GameConfigService } from "../game-config/game-config.service";
 
 @Injectable()
 export class TurretService {
 
   private readonly log = new Logger();
   private turrets: Turret[] = [];
-  constructor(private readonly conf: ConfigService) {
+  constructor(private readonly gameConfigService: GameConfigService) {
     this.buildTurrets();
   }
 
   private makeTurret(): Turret {
     return {
-      x: Math.floor(Math.random() * parseInt(this.conf.get('WIDTH'))),
-      y: Math.floor(Math.random() * parseInt(this.conf.get('HEIGHT'))),
+      x: Math.floor(Math.random() * this.gameConfigService.getConfig().width),
+      y: Math.floor(Math.random() * this.gameConfigService.getConfig().height),
       rotationSpeed: Math.random() * 0.5,
       firingRate: 40 + Math.floor(Math.random() * 30),
       rotation: Math.random() * Math.PI,
@@ -32,8 +32,9 @@ export class TurretService {
   public buildTurrets(): void {
     this.log.log('CREATING TURRETS');
     this.turrets = [];
-    for (let i = 0; i < parseInt(this.conf.get('TURRETS')); i++) {
+    for (let i = 0; i < this.gameConfigService.getConfig().turrets; i++) {
       this.turrets.push(this.makeTurret());
     }
+    this.log.log(this.turrets);
   }
 }
