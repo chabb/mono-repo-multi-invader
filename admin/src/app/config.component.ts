3afc2d01e4b8b8b80499ea69ab99627c0ec791aa
U120655 <U120655@mobi.ch>
2023-11-05 11:28:15 +0100

    fix: forms

--- admin/src/app/config.component.ts
+++ admin/src/app/config.component.ts
@@ -4,6 +4,7 @@ import { AdminService } from "./admin.service";
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { Config } from "@chabb/shared";
 import {Subject, switchMap, tap} from "rxjs";
+import {HttpClientModule} from "@angular/common/http";
 
 export type SimpleFormGroup<Type> = {
   [Property in keyof Type]: FormControl<Type[Property]>;
@@ -12,9 +13,9 @@ export type SimpleFormGroup<Type> = {
 @Component({
   selector: 'multi-invader-config',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   template: `
-    <form formGroupName="formGroup">
+    <form [formGroup]="formGroup">
       <input type="number"  formControlName="width">
       <input type="number"  formControlName="height">
       <input type="number"  formControlName="maxLife">
@@ -23,6 +24,8 @@ export type SimpleFormGroup<Type> = {
 
     <button (click)="save()"> {{isEditing ? 'Save' : 'Edit' }} </button>
     <button (click)="reset()" *ngIf="isEditing"> Cancel </button>
+
+    {{ config$ | async | json }}
   `,
   styles: [],
   encapsulation: ViewEncapsulation.None,
